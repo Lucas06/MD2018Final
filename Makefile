6d498525f155cbb23a55b524fece0e9d6e58911d
Jenaro Calviño <jen.calvineo@live.com.ar>
2018-04-02 15:35:04 -0300

    funcion random

---
+++ Makefile
@@ -0,0 +1,5 @@
+prueba: proyecto.c proyecto.h
+		gcc -o prueba proyecto.c
+
+clean:
+		rm prueba
--- proyecto.c
+++ proyecto.c
@@ -33,7 +33,7 @@ struct _Vertice_t {
 	Vecindad *vecinos;
 };
 
-typedef struct _Vertice_t *Vertice; 
+typedef struct _Vertice_t *Vertice;
 
 struct _Grafo_t
 {
@@ -45,9 +45,21 @@ struct _Grafo_t
 	Vertice vertice_array[];
 };
 
-typedef struct _Grafo_t *Grafo; /* *Grafo será un puntero a la estructura struct _Grafo_t */ 
+typedef struct _Grafo_t *Grafo; /* *Grafo será un puntero a la estructura struct _Grafo_t */
 
-/* Funciones de comparación */  
+/* Funcion generadora de número pseudo-random */
+
+/*
+u16 lfsr = 0xACE1u;
+u16 bit;
+
+ unsigned rand()
+ {
+   bit  = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) ) & 1;
+   return lfsr =  (lfsr >> 1) | (bit << 15);
+ }
+*/
+/* Funciones de comparación */
 
 int comparar_vertices_u(const void* a, const void* b) {
       /* Casteo argumentos a punteros a la estructura 'lado'. */
@@ -81,21 +93,21 @@ int comparar_vertices_v(const void* a, const void* b) {
 
 /* Alloca memoria para un Nuevo Grado leido por entrada estándar */
 
-Grafo ConstruccionDelGrafo() { 
-	
+Grafo ConstruccionDelGrafo() {
+
 	/* pedido de memoria para el grado */
 	Grafo mi_grafo = calloc(1, sizeof(struct _Grafo_t));
 	mi_grafo->nro_vertices = 0;
 	mi_grafo->nro_aristas = 0;
 	mi_grafo->nro_colores = 0;
-	
+
 	/* empezamos a leer el grafo por stdin */
 
 	char linea[BUFFER_SIZE];
-	u32 n_vertices, m_lados = 0; 
-    u32 vertice_u, vertice_v = 0; 
-    while(fgets(linea, sizeof(linea), stdin)) {   
-         if(strncmp(linea, "c", 1)) { 
+	u32 n_vertices, m_lados = 0;
+    u32 vertice_u, vertice_v = 0;
+    while(fgets(linea, sizeof(linea), stdin)) {
+         if(strncmp(linea, "c", 1)) {
             if(!strncmp(linea, "p edge ", 7)) {
                sscanf(linea, "%*c %*s %u %u", &n_vertices, &m_lados);
                break;
@@ -107,16 +119,16 @@ Grafo ConstruccionDelGrafo() {
 	printf("Esto es una prueba y he leido correctamente el numero de vertices y el numero de lados \n");
 	mi_grafo->nro_vertices = n_vertices;
 	mi_grafo->nro_aristas = m_lados;
-	
+
 	printf("Este grafo tiene %u vertices \n", mi_grafo->nro_vertices);
 	printf("Este grafo tiene %u aristas \n", mi_grafo->nro_aristas);
 	printf("Este grafo tiene %u colores \n", mi_grafo->nro_colores);
 
 	/* --------------------------------------------------------------------------------------------------- */
 
-         u32 e_lineas = 0; 
+         u32 e_lineas = 0;
 
-         /* 
+         /*
             Creamos un array de 2 * M lados donde se van a guardar las lecturas de cada lado
             de manera espejada. Es decir, tanto el lado u-v como v-u.
             La idea es tener una estructura intermedia donde se alojen los lados de manera
@@ -130,7 +142,7 @@ Grafo ConstruccionDelGrafo() {
          lado* espejo_vu = NULL; // 'Lado backward' v-u.
          u32 indice_espejado = 0; // Indice que recorre el arreglo de lados espejados.
 
-         Vertice vertice = malloc(n_vertices*sizeof(struct _Vertice_t)); // arreglo de vertices 
+         Vertice vertice = malloc(n_vertices*sizeof(struct _Vertice_t)); // arreglo de vertices
          u32 indice_vertice = 0;
 
          while(fgets(linea, sizeof(linea), stdin)) {
@@ -153,7 +165,7 @@ Grafo ConstruccionDelGrafo() {
                e_lineas += 1;
             }
          }
-         assert(e_lineas == m_lados); 
+         assert(e_lineas == m_lados);
          assert(indice_espejado == size_espejada);
          qsort(lados_espejados, size_espejada, sizeof(struct lado_t), comparar_vertices_u);
          size32 size_vecindad = n_vertices; //en el peor de los casos
@@ -179,7 +191,7 @@ Grafo ConstruccionDelGrafo() {
          	vertice[i].color = 0;
          	//vertice[i].grado = vecindad[i].grado;
          	vertice[i].vecinos = NULL;
-         	mi_grafo->vertice_array[i] = &vertice[i]; //estoy llenando el arreglo de punteros 
+         	mi_grafo->vertice_array[i] = &vertice[i]; //estoy llenando el arreglo de punteros
          	/* chequeo */
          	//printf("Hola soy el vertice con nombre real %u, etiqueta %u, color %u y grado %u\n", vertice[i].nombre_real, vertice[i].etiqueta, vertice[i].color, vertice[i].grado);
             printf("Hola soy el vertice con nombre real %u, etiqueta %u y color %u\n", vertice[i].nombre_real, vertice[i].etiqueta, vertice[i].color);//, vertice[i].grado);
@@ -212,7 +224,7 @@ Grafo ConstruccionDelGrafo() {
                printf("Vertice %u es vecino de %u\n", vecindad[i].vertice, vecindad[i].puntero[j]);
             }
          }
-         free(lados_espejados); /* Destruyo los lados espejados */ 
+         free(lados_espejados); /* Destruyo los lados espejados */
          for (int i = 0; i < size_vecindad; ++i) {
                free(vecindad[i].puntero);
          }
@@ -229,7 +241,7 @@ u32 NumeroDeVertices(Grafo G) {
 }
 u32 NumeroDeLados(Grafo G) {
 	return G->nro_aristas;
-} 
+}
 u32 NumeroDeColores(Grafo G) {
 	return G->nro_colores;
 }
@@ -238,27 +250,27 @@ u32 NumeroDeColores(Grafo G) {
 
 
 u32 NombreDelVertice(Grafo G, u32 i){
-	return (G->vertice_array[i]->nombre_real);	
+	return (G->vertice_array[i]->nombre_real);
 }
 /*
 u32 ColorDelVertice(Grafo G, u32 i) {
 	return (G->vertice_array[i]->color);
 }
 u32 GradoDelVertice(Grafo G, u32 i) {
-	return (G->vertice_array[i]->grado);	
+	return (G->vertice_array[i]->grado);
 }
 u32 ColorJotaesimoVecino(Grafo G, u32 i, u32 j) {
-	return (G->vertice_array[i]->vecinos[j].color);	
+	return (G->vertice_array[i]->vecinos[j].color);
 }
 u32 NombreJotaesimoVecino(Grafo G, u32 i,u32 j) {
-	return (G->vertice_array[i]->vecinos[j].vertice);	
+	return (G->vertice_array[i]->vecinos[j].vertice);
 }c
 */
 
 /* -------------------------------------------------------------------------------- */
 
 
-/* ---------------------------------------------------------------------------------------------------------------- */  
+/* ---------------------------------------------------------------------------------------------------------------- */
 
 int main (void) {
 	Grafo mi_grafo = NULL;
@@ -278,4 +290,3 @@ int main (void) {
 	// printf("estoy probando la función NombreJotaesimoVecino con etiqueta 1 = %u\n", NombreJotaesimoVecino(mi_grafo, 1, 1));
 	return 0;
 }
-
